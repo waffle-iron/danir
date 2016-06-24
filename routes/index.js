8f999e4024f20c52c234f4630660cb3a345ee635
Dmitry <chernyavsky_work@mail.ru>
2016-06-24 10:33:08 +0300

    Added transitions, finished contacts page

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Danir.by' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Danir.by' });
 });
 
 
 router.get('/marketing',function (req,res) {
-  res.render('marketing',{title:"Marketing"})
+  res.render('marketing',{title:"Danir.by"})
 });
 
 
 router.get('/contacts',function (req,res) {
-  res.render('contacts',{title:"Contacts"})
+  res.render('contacts',{title:"Danir.by"})
 });
 
 
